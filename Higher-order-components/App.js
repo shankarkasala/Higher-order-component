9de15708ba4330aed1933d799293f634b96789aa
shankarkasala <80156261+shankarkasala@users.noreply.github.com>
2021-03-09 19:20:57 +0530

    initial commit

---
+++ Higher-order-components/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+import Clickcounter from './Higher-order-components/Clickcounter';
+import Hovercounter from './Higher-order-components/Hovercounter';
+
+
+
+function App() {
+  return (
+    <div className="App">
+    <Clickcounter/>
+  <Hovercounter/>
+    </div>
+  );
+}
+
+export default App;
